75ffbae6e522aed24cc72b39e9d609051c6b04dd
SlavaMryasov <slavadevbox1@gmail.com>
2024-03-25 17:43:12 +0300

    hw1

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const user = { name: 'John', age: 25, city: 'New York' }; 
+  // console.log(user.age)
+  // console.log(user.city)
+  // console.log(user.name)
+
+  // const fruits = ['apple', 'banana', 'orange']
+  // console.log(fruits[1])
+  // console.log(fruits[2])
+
+
+//   const person = { name: 'Alice', age: 30 }; 
+// // Деструктуризация в параметрах функции 
+// function printPersonDetails({name, age}: any) { 
+//  console.log(`Name: ${name}, Age: ${age}`); 
+// } 
+// printPersonDetails(person);
+
+
+// const car = { brand: 'Toyota', model: 'Camry', year: 2022 }; 
+// console.log(car.brand);  // 'Toyota' 
+// console.log(car.model);  // 'Camry' 
+// console.log(car.year); 
+
+
+// const students = [ 
+//   { name: 'Alex', grade: 'A' }, 
+//   { name: 'Emma', grade: 'B' }, 
+//   { name: 'Chris', grade: 'C' }, 
+//  ]; 
+// console.log(students[0].name)
+// console.log(students[2].name)
+
+
+// const person: any = { firstName: 'Max', lastName: 'Johnson' }; 
+// person.age = 25
+// person.firstName='Nina'
+// person.lastName='Ivanov'
+// console.log(person.firstName)
+// console.log(person.lastName)
+// console.log(person.age)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
